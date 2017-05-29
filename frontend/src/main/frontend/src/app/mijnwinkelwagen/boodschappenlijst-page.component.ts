c5a33a6bc082194d57897ca023933f86f51e5422
Aalten <richard.aalten@rabobank.nl>
2017-05-29 22:35:03 +0200

    Latest changes

--- frontend/src/main/frontend/src/app/mijnwinkelwagen/boodschappenlijst-page.component.ts
+++ frontend/src/main/frontend/src/app/mijnwinkelwagen/boodschappenlijst-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mijnwinkelwagen-page',
-  templateUrl: './boodschappenlijst.component.html'
+  templateUrl: './overview/boodschappenlijst.component.html'
 })
 export class MijnWinkelwagenPageComponent {
   constructor() {}
